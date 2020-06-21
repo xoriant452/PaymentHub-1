4a603193e398b32896f2a37dcbcebe11052aed94
kadam_sy <kadam_sy@Sayali-KM.India.XoriantCorp.com>
2020-06-21 18:46:45 +0530

    Maven Project is created.

---
+++ devOps-maven-project/src/test/java/simpleProject/DevOpsClass.java
@@ -0,0 +1,13 @@
+package simpleProject;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DevOpsClass {
+
+	 @Test
+	    public void test() {
+	        Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+	    }
+	 
+}
