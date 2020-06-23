7a37fd0927cc78862223c6b811b582ccd0a78939
C5300646 <C5300646@WDFV41002488D.emea.global.corp.sap>
2020-06-23 12:23:41 +0530

    code changes

--- src/test/java/com/devops/session1/assignment1/CICD.java
+++ src/test/java/com/devops/session1/assignment1/CICD.java
@@ -4,7 +4,7 @@ public class CICD {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10,b=20,c;
+		int a=30,b=20,c;
 		c=a+b;
 		System.out.println("value of C is :"+c);
 
