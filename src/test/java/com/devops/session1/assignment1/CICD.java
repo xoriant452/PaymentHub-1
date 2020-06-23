ea132871052b4b08ab861aaf6bdc3cc457ca2d64
C5300646 <C5300646@WDFV41002488D.emea.global.corp.sap>
2020-06-23 13:07:12 +0530

    Added loops

--- src/test/java/com/devops/session1/assignment1/CICD.java
+++ src/test/java/com/devops/session1/assignment1/CICD.java
@@ -7,6 +7,14 @@ public class CICD {
 		int a=50,b=20,c;
 		c=a+b;
 		System.out.println("value of C is :"+c);
+		if(a>b)
+		{
+			System.out.println("a is greater than b");
+		}
+		else
+		{
+			System.out.println("b is greater than a");
+		}
 
 	}
 
