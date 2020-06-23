5da88c98ea4cb048ad4b69a7645a6a3aadbeeeb1
C5300646 <C5300646@WDFV41002488D.emea.global.corp.sap>
2020-06-23 12:19:34 +0530

    testNc

---
+++ src/test/java/com/devops/session1/assignment1/CICD.java
@@ -0,0 +1,13 @@
+package com.devops.session1.assignment1;
+
+public class CICD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("value of C is :"+c);
+
+	}
+
+}
