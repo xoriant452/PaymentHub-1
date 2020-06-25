09b6ea3231118d42e4091a7e6647beb72458b757
Shivam Vishwakarma <vishwakarma_sh@India.XoriantCorp.com>
2020-06-25 22:35:21 +0530

    first-commit

---
+++ openshift-demo/src/main/java/openshiftdemo/OpenshiftDemoApplication.java
@@ -0,0 +1,19 @@
+package openshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class OpenshiftDemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenshiftDemoApplication.class);
+	}
+}
